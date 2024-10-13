c1ab497d53d69a9b9c34863a05e2a119c5d87e37
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-10-13 09:06:52 -0500

    Usando soap

---
+++ src/main/java/pe/edu/cibertec/ws_soap_basedatos/WsSoapBasedatosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_soap_basedatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSoapBasedatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSoapBasedatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/ws_soap_basedatos/WsSoapBasedatosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ws_soap_basedatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSoapBasedatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
